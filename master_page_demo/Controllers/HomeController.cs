d66baba1312538ca01c272056112a24bce3b2b83
Kumari <jyoti.kumari@nuance.com>
2019-07-23 14:39:16 +0530

    all files added

--- master_page_demo/Controllers/HomeController.cs
+++ master_page_demo/Controllers/HomeController.cs
@@ -26,5 +26,46 @@ namespace master_page_demo.Controllers
 
             return View();
         }
+        public ActionResult About_game()
+        {
+            return View("About_game");
+        }
+        public ActionResult contact_game()
+        {
+            return View("contact_game");
+        }
+        [HttpGet]
+        public ViewResult Membership()
+        {
+            return View();
+        }
+        public ViewResult game2()
+        {
+            return View("game2");
+        }
+        [HttpPost]
+        public ViewResult Membership(master_page_demo.Models.Member formdata)
+        {
+            //database
+            return View("Thanks", formdata);
+        }
+        [HttpGet]
+        public ViewResult login_page()
+        {
+            //Models.Member loginModel = new Models.Member();
+            return View("login_page");
+        }
+       [HttpPost]
+        public ViewResult login_page(master_page_demo.Models.Member logindata)
+        {
+            if (logindata.email_id == "jyoti.kumari@gmail.com" && logindata.password == "1234")
+
+                return View("conformation", logindata);
+            else
+            {
+                ViewBag.Message = "login not sucessfull check id and password";
+                return View("login_page");
+            }
+        }
     }
 }
---
+++ master_page_demo/Models/Member.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace master_page_demo.Models
+{
+    public class Member
+    {
+        public String first_name { get; set; }
+        public String last_name { get; set; }
+        public String email_id { get; set; }
+        public String address { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string password { get; set; }
+        public string conf_password { get; set; }
+        public string phone_no { get; set; }
+      
+
+    }
+}
